6c09a5be29861baee9849db0e780f3ef9437a105
Vidwa De Seram <vidwa@xaventra.com>
2025-02-01 11:05:47 +0530

    neorg added

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -52,4 +52,10 @@ return {
       require("telescope").load_extension("import")
     end,
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
 }
