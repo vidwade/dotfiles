baf4eb1dffbdb18d4500f7d8e8418968794a66ab
Vidwa De Seram <vidwanirmaldeseram@gmail.com>
2026-01-02 09:46:33 +0530

    git diff

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -144,4 +144,13 @@ return {
       opts = { diagnostics = { virtual_text = false } },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("gitsigns").setup({
+        word_diff = true, -- inline word diff
+      })
+    end,
+  },
 }
